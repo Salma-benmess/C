225c7a94258984bb4184dba0efddcdf3471578d4
salma <salmabnemess@gmail.com>
2023-07-23 17:42:34 -0400

    function

---
+++ ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+ 
+void ft_print_numbers(){
+
+
+	char num='1';
+	while(num <= '9'){
+		write(1,&num,1);
+		num++;
+	}
+}
+int main() {
+	ft_print_numbers();
+	return 0 ;
+}
+      	
+
+
+	
+
