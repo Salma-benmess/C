46e2b26fccc10723ebd437d62d58f501b33059c8
salma <salmabnemess@gmail.com>
2023-07-24 15:30:52 -0400

    function

--- ft_is_negative.c
+++ ft_is_negative.c
@@ -1,15 +1,17 @@
 #include <unistd.h>
 
 void ft_is_negative (int a){
-	while(a<0){
+	if (a<0){
 		char S ='N';
-		write(1,&S,1);
+		write(1, &S ,1);
 	}
-	char S ='P';
-	write(1, &S ,1);
+	else {
+		char S ='P';
+	        write(1, &S ,1);
+	}
 }
 int main(){
-	ft_is_negative(5);
+	ft_is_negative(-5);
 	return 0 ;
 }
 
